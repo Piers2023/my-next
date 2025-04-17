d93fd2430a005b8b06b4bc9cfda75c5eb4049dd2
Piers2023 <peacepimpee@gmail.com>
2025-04-17 14:32:33 +0700

    Update page.js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,7 +2,7 @@ export default function AboutPage() {
     return (
       <main className="p-6">
         <h1 className="text-2xl font-bold">About Me</h1>
-        <p>I'm learning Next.js fullstack in 7 days 🎯</p>
+        <p>I'm learning Next.js fullstack in 7 days test 🎯</p>
       </main>
     );
   }
